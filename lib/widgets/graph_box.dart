1603e00b7156867cc258aa92a298caab4e0259e2
YuraDnistriansky <yura.dniester@gmail.com>
2026-03-12 00:08:20 +0200

    mini fix

--- lib/widgets/graph_box.dart
+++ lib/widgets/graph_box.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
 
 // Поки нехай так, щоб просто якийсь графік був, потім це перенеситься
 class GraphPoint {
