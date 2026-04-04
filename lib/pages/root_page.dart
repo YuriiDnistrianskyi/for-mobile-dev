7aafe09a92cf515f05077dc3edf29eeaa8dd625b
YuraDnistriansky <yura.dniester@gmail.com>
2026-04-04 18:46:55 +0300

    add messenger in root page

--- lib/pages/root_page.dart
+++ lib/pages/root_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:my_project/pages/home_page.dart';
 import 'package:my_project/pages/login_page.dart';
 import 'package:my_project/providers/auth_provider.dart';
+import 'package:my_project/providers/wifi_provider.dart';
 import 'package:provider/provider.dart';
 
 class RootPage extends StatefulWidget {
@@ -15,7 +16,18 @@ class _RootPageState extends State<RootPage> {
   @override
   void initState() {
     super.initState();
-    context.read<AuthProvider>().autoLogin();
+    _initRootPage();
+  }
+
+  Future<void> _initRootPage() async {
+    final auth = context.read<AuthProvider>();
+    await auth.autoLogin();
+    if (!mounted) return;
+    if (!context.read<WiFiProvider>().isConnected && auth.isLoggin) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Not conneted'))
+      );
+    }
   }
 
   @override
