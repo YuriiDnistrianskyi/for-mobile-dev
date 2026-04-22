6ab5037e22aa75868eb7f224d8e2ce5237ffd9db
YuraDnistriansky <yura.dniester@gmail.com>
2026-04-22 22:18:37 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,18 @@ import 'package:my_project/core/api/api_service.dart';
 import 'package:my_project/core/mqtt_manager.dart';
 import 'package:my_project/core/token_store.dart';
 import 'package:my_project/pages/root_page.dart';
-import 'package:my_project/providers/auth_provider.dart';
-import 'package:my_project/providers/device_provider.dart';
-import 'package:my_project/providers/object_provider.dart';
-import 'package:my_project/providers/speed_graph_provider.dart';
-import 'package:my_project/providers/temperature_graph_provider.dart';
-import 'package:my_project/providers/user_provider.dart';
-import 'package:my_project/providers/wifi_provider.dart';
+// import 'package:my_project/providers/auth_provider.dart';
+// import 'package:my_project/providers/device_provider.dart';
+// import 'package:my_project/providers/object_provider.dart';
+// import 'package:my_project/providers/speed_graph_provider.dart';
+// import 'package:my_project/providers/temperature_graph_provider.dart';
+// import 'package:my_project/providers/user_provider.dart';
+// import 'package:my_project/providers/wifi_provider.dart';
 import 'package:my_project/repository/device_repository.dart';
 import 'package:my_project/repository/general_repository.dart';
-import 'package:my_project/repository/graph_repository.dart';
+// import 'package:my_project/repository/graph_repository.dart';
 import 'package:my_project/repository/object_repository.dart';
-import 'package:my_project/repository/user_repository.dart';
+// import 'package:my_project/repository/user_repository.dart';
 import 'package:my_project/services/mqtt_service.dart';
 import 'package:my_project/services/notification_service.dart';
 import 'package:my_project/widgets/app_background.dart';
@@ -79,57 +79,10 @@ class MyApp extends StatelessWidget {
         Provider<ObjectRepository>(
           create: (_) => ObjectRepository(db: db, api: apiService),
         ),
+        Provider<MqttService>(
+          create: (context) => service,
+        )
 
-        ChangeNotifierProvider(
-          create: (_) => AuthProvider(
-            repository: UserRepository(db: db, api: apiService),
-            tokenStore: tokenStore,
-            apiClient: apiClient
-          ),
-        ),
-        ChangeNotifierProvider(
-          create: (context) => DeviceProvider(
-            repository: context.read<DeviceRepository>(),
-            mqttService: service,
-          ),
-        ),
-        ChangeNotifierProvider(
-          create: (context) => ObjectProvider(
-            repository: context.read<ObjectRepository>(),
-            mqttService: service,
-          ),
-        ),
-        ChangeNotifierProvider(
-          create: (context) {
-            final provider = SpeedGraphProvider(
-              repository: GraphRepository(db: db, api: apiService),
-              deviceRepository: context.read<DeviceRepository>(),
-            );
-            provider.listen(service);
-            return provider;
-          },
-        ),
-        ChangeNotifierProvider(
-          create: (context) {
-            final provider = TemperatureGraphProvider(
-              repository: GraphRepository(db: db, api: apiService),
-              objectRepository: context.read<ObjectRepository>(),
-            );
-            provider.listen(service);
-            return provider;
-          },
-        ),
-        ChangeNotifierProvider(
-          create: (_) => UserProvider(
-            repository: UserRepository(db: db, api: apiService),
-          ),
-        ),
-        ChangeNotifierProvider(
-          create: (_) {
-            final provider = WiFiProvider();
-            return provider;
-          },
-        ),
       ],
       child: MaterialApp(
         title: 'Cooling System',
