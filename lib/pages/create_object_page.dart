fb32271063cc6092a3548b7bea6d63600ad0273c
YuraDnistriansky <yura.dniester@gmail.com>
2026-04-22 23:07:30 +0300

    Update create_object_page.dart

--- lib/pages/create_object_page.dart
+++ lib/pages/create_object_page.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_project/cubit/object/object_cubit.dart';
+import 'package:my_project/cubit/object/object_state.dart';
 import 'package:my_project/models/object_model.dart';
 import 'package:my_project/providers/auth_provider.dart';
-import 'package:my_project/providers/object_provider.dart';
 import 'package:my_project/widgets/form_layer.dart';
 import 'package:my_project/widgets/object_fields.dart';
-// import 'package:my_project/widgets/custom_field.dart';
-// import 'package:my_project/widgets/important_button.dart';
-// import 'package:my_project/widgets/password_field.dart';
-// import 'package:my_project/widgets/title_page_text.dart';
-import 'package:provider/provider.dart';
 
 class CreateObjectPage extends StatefulWidget {
   final bool isCreate;
@@ -36,26 +33,24 @@ class _CreateObjectPageState extends State<CreateObjectPage> {
     super.initState();
 
     if (!widget.isCreate) {
-      context.read<ObjectProvider>().getObject(widget.id!);
-      final MyObject object = context.read<ObjectProvider>().object!;
-
-      _publicNameController.text = object.publicName;
-      _privateNameController.text = object.privateName;
-      _maxTemperatureComtroller.text = object.maxTemperature.toString();
-      _defaulSpeedController.text = object.defaultSpeedForDevices.toString();
+      context.read<ObjectCubit>().getObject(widget.id!);
     }
-  }
+   }
+
+  // super.initState();
+
+  // if (!widget.isCreate) {
+  //   context.read<ObjectCubit>().getObject(widget.id!);
+  // }
 
   void _action() async {
-    final objectProvider = context.read<ObjectProvider>();
+    final cubit = context.read<ObjectCubit>();
     final authProvider = context.read<AuthProvider>();
 
     if (_formKey.currentState!.validate()) {
       if (widget.isCreate) {
         final privateName = _privateNameController.text.trim();
-        final bool objectExists = await objectProvider.objectExists(
-          privateName,
-        );
+        final bool objectExists = await cubit.objectExists(privateName);
 
         if (!mounted) return;
 
@@ -73,7 +68,7 @@ class _CreateObjectPageState extends State<CreateObjectPage> {
           );
           return;
         }
-        await objectProvider.createObject(
+        await cubit.createObject(
           _publicNameController.text,
           _privateNameController.text,
           _passwordController.text,
@@ -82,8 +77,8 @@ class _CreateObjectPageState extends State<CreateObjectPage> {
           int.parse(_defaulSpeedController.text),
         );
       } else {
-        final MyObject currentObject = context.read<ObjectProvider>().object!;
-        await objectProvider.updateObject(
+        final MyObject currentObject = cubit.state.object!;
+        await cubit.updateObject(
           widget.id as int,
           _publicNameController.text.trim(),
           currentObject.privateName,
@@ -94,8 +89,8 @@ class _CreateObjectPageState extends State<CreateObjectPage> {
         );
 
         if (!mounted) return;
-        
-        context.read<ObjectProvider>().getObject(widget.id!);
+
+        cubit.getObject(widget.id!);
       }
 
       if (!mounted) return;
@@ -105,37 +100,49 @@ class _CreateObjectPageState extends State<CreateObjectPage> {
   }
 
   void _deleteObject() async {
-      final objectProvider = context.read<ObjectProvider>();
-      await objectProvider.deleteObject(
-        widget.id!,
-        context.read<AuthProvider>().userId!,
-      );
-      if (!mounted) return;
-      Navigator.pop(context);
-      ScaffoldMessenger.of(
-        context,
-      ).showSnackBar(const SnackBar(content: Text('Object deleted')));
-    }
+    final cubit = context.read<ObjectCubit>();
+    await cubit.deleteObject(widget.id!, context.read<AuthProvider>().userId!);
+    if (!mounted) return;
+    Navigator.pop(context);
+    ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(const SnackBar(content: Text('Object deleted')));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return FormLayer(
+    return BlocProvider.value(
+      value: context.read<ObjectCubit>(),
+      child: BlocListener<ObjectCubit, ObjectState>(
+        listener: (context, state) {
+          if (!widget.isCreate) {
+            // context.read<ObjectProvider>().getObject(widget.id!);
+            final MyObject object = state.object!;
+
+            _publicNameController.text = object.publicName;
+            _privateNameController.text = object.privateName;
+            _maxTemperatureComtroller.text = object.maxTemperature.toString();
+            _defaulSpeedController.text = object.defaultSpeedForDevices
+                .toString();
+          }
+        },
+        child: FormLayer(
       title: widget.isCreate ? 'Create Object' : 'Edit Object',
       backAction: () => Navigator.pop(context),
-      actions: widget.isCreate 
+      actions: widget.isCreate
           ? null
           : [
-            IconButton(
-              onPressed: _deleteObject,
-              icon: const Icon(Icons.delete, color: Colors.red)
-            )
-          ],
+              IconButton(
+                onPressed: _deleteObject,
+                icon: const Icon(Icons.delete, color: Colors.red),
+              ),
+            ],
       fields: ObjectFields(
         formKey: _formKey,
         publicNameController: _publicNameController,
         privateNameController: widget.isCreate ? _privateNameController : null,
         passwordController: widget.isCreate ? _passwordController : null,
-        confirmPasswordController: widget.isCreate 
+        confirmPasswordController: widget.isCreate
             ? _confirmPasswordController
             : null,
         maxTemperatureController: _maxTemperatureComtroller,
@@ -143,6 +150,8 @@ class _CreateObjectPageState extends State<CreateObjectPage> {
       ),
       textButton: widget.isCreate ? 'Create' : 'Edit',
       pressAction: _action,
+    )
+      ),
     );
   }
 }
