75dc6672e271efdf625f4b60cc8ed7579b9d5c7f
YuraDnistriansky <yura.dniester@gmail.com>
2026-04-25 00:52:03 +0300

    Create user_state.dart

---
+++ lib/cubit/user/user_state.dart
@@ -0,0 +1,31 @@
+import 'package:my_project/models/user_model.dart';
+
+class UserState {
+  final User? user;
+  final bool isLoading;
+  final String? error;
+
+  UserState({
+    required this.isLoading,
+    this.user,
+    this.error
+  });
+
+  factory UserState.initial() {
+    return UserState(
+      isLoading: false
+    );
+  }
+
+  UserState copyWith(
+    User? user,
+    bool? isLoading,
+    String? error,
+  ) {
+    return UserState(
+      isLoading: isLoading ?? this.isLoading,
+      user: user ?? this.user,
+      error: error ?? this.error,
+    );
+  }
+}
